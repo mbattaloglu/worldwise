1d65c5a004e9ce650f4870ae3cfdeef1f74ef4be
mbattaloglu <mmbattaloglu@gmail.com>
2024-03-14 02:49:09 +0300

    files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Worldwise
+From the Jonas Schmedtmann's course ["The Ultimate React Course 2024: React, Redux & More"](https://www.udemy.com/course/the-ultimate-react-course/).
---
+++ src/components/CityList.jsx
@@ -0,0 +1,28 @@
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
+
+function CityList() {
+  const { isLoading, cities } = useCities();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!cities.length)
+    return (
+      <Message message={"Add your first city by clicking a city on the map"} />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
